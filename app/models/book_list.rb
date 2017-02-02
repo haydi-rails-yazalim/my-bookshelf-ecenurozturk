2551af80006144c32334f0423c167ae8fa1472c4
ecenurozturk <ecenurrozturk@gmail.com>
2017-02-02 15:21:48 +0200

    model hata

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -1,2 +1,4 @@
 class BookList < ApplicationRecord
+  has_many :bookshelf
+  has_many :user,through: :bookshelf
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201182106) do
+ActiveRecord::Schema.define(version: 20170202130956) do
 
   create_table "book_lists", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170201182106) do
     t.integer  "number_of_pages"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "bookshelves", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
